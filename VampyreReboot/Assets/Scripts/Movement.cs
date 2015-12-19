4f745bb5b2fad93780e91a9837c477f6d1e5f4b9
Mark Plant <gothicsyn@live.co.uk>
2015-12-19 00:52:49 +0000

    Controller Updates
    Started Work On The Character Controller Using Burgzerg Arcades Root
    Motion Tutorials. Also fixed a stupid bug that was annoying me. Tutorial
    2 Complete.

---
+++ VampyreReboot/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class Movement : MonoBehaviour {
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        // Commands for Turning, Jumping, moving
+        Move();
+    }
+
+    void Move()
+    {
+        anim.SetFloat("Forward", Input.GetAxisRaw("Vertical"));
+    }
+}
