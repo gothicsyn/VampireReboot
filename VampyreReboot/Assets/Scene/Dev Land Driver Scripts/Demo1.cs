3310496fee4e7c432e6828774a22fa99d850db6e
Mark Plant <gothicsyn@live.co.uk>
2016-03-27 15:17:40 +0100

    Couple of Resets
    Put the spawner back in, reset DevLand, done some work on the
    controller, to around tutorial 21 of BZA

---
+++ VampyreReboot/Assets/Scene/Dev Land Driver Scripts/Demo1.cs
@@ -0,0 +1,37 @@
+﻿//Connect to the Database
+//Spawn Items From The Database
+
+using UnityEngine;
+using System.Collections;
+using BurgZergArcade.ItemSystem;
+
+[DisallowMultipleComponent]
+public class Demo1 : MonoBehaviour {
+	public ItemSystemWeaponDatabase database;
+
+	void OnGUI() 
+	{
+		for (int cnt = 0; cnt < database.Count; cnt++)
+			if (GUILayout.Button ("Spawn: " + database.Get (cnt).Name))
+				Spawn (cnt);
+	}
+
+
+	void Spawn(int index)
+	{
+		ItemSystemWeapon isw = database.Get (index);
+
+		GameObject weapon = Instantiate (isw.Prefab);
+		weapon.name = isw.Name;
+
+		Weapon myWeapon = weapon.AddComponent<Weapon> ();
+		myWeapon.Icon = isw.Icon;
+		myWeapon.Value = isw.Value;
+		myWeapon.Burden = isw.Burden;
+		myWeapon.Quality = isw.Quality.Icon;
+		myWeapon.Min_Damage = isw.minDamage;
+		myWeapon.Min_Durability = isw.Durability;
+		myWeapon.Max_Durability = isw.MaxDurability;
+		myWeapon.Equipment_Slot = isw.equipmentSlot;
+	}
+}
